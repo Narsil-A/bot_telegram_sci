4a17f8c888ea30aab95d8d9eda1d137a434212e5
Andrea Camargo <andreabyq1991@gmail.com>
2022-01-24 20:53:26 -0400

    new url for credentials file

--- Telebot/credentials.py
+++ Telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1905773517:AAG4dRSOTNEtgp7YHWKO2RGRxI-kga4WEiY"
 bot_user_name = "Science_Dictionary_Bot"
-URL = "https://science-words.herokuapp.com/"
+URL = " https://sciences-words.herokuapp.com/"
